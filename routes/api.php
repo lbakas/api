8c1cd2f95a3f769aa51b5a7c8d94eb8ff7d15fe6
Laurynas Bakas <laurynas.bakas@gmail.com>
2021-06-06 15:25:03 +0300

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/users', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/register', 'App\Http\Controllers\API\AuthController@register');
 Route::post('/login', 'App\Http\Controllers\API\AuthController@login');
 
